f0874e06a6fa5de28deb0e3c69e21b85d8e1e3d2
Margaret Huang <margart.huang@pioneer.com>
2018-03-09 11:07:55 -0800

    Add first working SPA

--- src/App.js
+++ src/App.js
@@ -1,1 +1,6 @@
 // App.js will go here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var markupExample = React.createElement('h1', {}, 'lolol')
+ReactDOM.render(markupExample, document.getElementById('root'))
